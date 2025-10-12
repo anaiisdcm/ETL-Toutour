885ff2809b2b625ccebbbf18a9b3badd97f5b499
Niels Albrecht <146889920+nnniels@users.noreply.github.com>
2025-10-12 09:51:48 +0200

    Adding comments

--- DataGeneration/make_noise.py
+++ DataGeneration/make_noise.py
@@ -2,15 +2,29 @@ import numpy as np
 import numpy.random as rd
 import pandas as pd
 
-file_in = "./data_out/dogs_dataset.csv"
-file_in = "./data_in/demo.csv"
+
+"""
+This file contains function to add noise to pandas dataframe.
+"""
+
+# load pandas dataframe from generated csv
+# file_in = "./data_out/dogs_dataset.csv"
+file_in = "./data_in/demo.csv" # file for testing
 file_out = "./data_noisy/WTF.csv"
 
 df_in = pd.read_csv(file_in)
 
 
 
-
+"""
+Function to nullify several rows. Rows are randomly chosen. Editing is in place.
+All values of the chosen rows will be None or NaN.
+Args :
+    df : pandas dataframe whose rows will be nullified
+    n_rows : the number of row to nullify
+Returns :
+    df with n_rows randomly chosen null rows
+"""
 def NullRowMaker(df, n_rows):
     r, c = df.shape
     if n_rows >= r//2 :
@@ -22,7 +36,12 @@ def NullRowMaker(df, n_rows):
     df.loc[r_to_null] = None
     return df
 
-
+"""
+Function to replace randomly chosen values by None or NaN. Editing is in place.
+Ags :
+    df : pandas dataframe to replace values by None
+    n_nulls : total number of values to replace by None or Nan
+"""
 def RandomNullMaker(df, n_nulls):
     r, c = df.shape
     if n_nulls >= r*c/5 :
@@ -37,7 +56,15 @@ def RandomNullMaker(df, n_nulls):
         df.loc[ r_to_null[i] , df.columns.values[c_to_null[i]] ] = None
     return df
     
-
+"""
+Function to insert fake values into a selection of columns. 
+Automatically handle columns type (numeric or not). Editing is in place.
+Replacement values are computed to be at least 4 std deviation from mean. Can be negative.
+Args :
+    df : dataframe to edit
+    columns : selection of columns in which to tweak values in
+    n_faked_c : nummber of values to tweak in each column
+"""
 def ExageratedValues(df, columns, n_faked_c):
     r, c = df.shape
     for c in columns :
@@ -60,16 +87,15 @@ def ExageratedValues(df, columns, n_faked_c):
         
 
 
+"""
+Test section
+"""
 print(df_in)
+print()
 
-# df_1 = NullRowMaker(df_in, 1)
-# print(df_1)
-
-# df_2 = RandomNullMaker(df_in, 3)
-# print(df_2)
-
-df_3 = ExageratedValues(df_in, ['yy', 'C'], 2)
-print(df_3)
+# print(NullRowMaker(df_in, 1))
+# print(RandomNullMaker(df_in, 3))
+print(ExageratedValues(df_in, ['yy', 'C', 'A'], 2))
 
 
 
