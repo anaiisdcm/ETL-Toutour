6f8f1f318f9538f8eda263deb5d7455d01771606
Niels <niels.albrecht.474@gmail.com>
2025-10-18 15:21:25 +0200

    génération de toutes les données avec les bons types

--- DataGeneration/generate_all_v2.py
+++ DataGeneration/generate_all_v2.py
@@ -5,25 +5,58 @@ import random as rd
 import string
 
 
+# for reproductibility
+nrd.seed(33)
 
 """ 
 Wether to generate each csv file
 """
 dog = True
-propriétaire = True
-promeneur = True
-promeneur_dispo = True
-promenade_passées = True
-note_promeneur = True
-paiements_propriétaires = True
-promenade_requests = True
-notes_chien = True
+owner = True
+walker = True
+walker_availability = True
+past_walks = True
+walker_review = True
+owner_payment = True
+walk_requests = True
+dog_review = True
 
+"""
+Numbers numbers
+"""
+n_past_walk = 200
 
 
 """
 Generate all required csv
 """
+
+if owner :
+    # --- owners 
+    n_owner = 100
+    
+    last_name = rd.choices(["Martin", "Bernard", "Dubois", "Thomas", "Robert", "Brunissende"], k=n_owner)
+    first_name = rd.choices(["Jean", "Marie", "Pierre", "Sophie", "Luc", "Octave", "Odilon", "Odon", "Oger", "Olivier", "Oury", "Pacôme", "Palémon", "Parfait", "Pascal", "Paterne", "Patrice", "Paul", "Pépin", "Perceval", "Philémon", "Philibert", "Philippe", "Philothée", "Pie", "Pierre", "Pierrick", "Prosper", "Quentin", "Raoul", "Raphaël", "Raymond", "Régis", "Réjean", "Rémi", "Renaud", "René", "Reybaud", "Richard", "Robert", "Roch", "Rodolphe", "Rodrigue", "Roger", "Roland", "Romain", "Romuald", "Roméo", "Rome", "Ronan", "Roselin", "Salomon", "Samuel", "Savin", "Savinien", "Scholastique", "Sébastien", "Séraphin", "Serge", "Séverin", "Sidoine", "Sigebert", "Sigismond", "Silvère", "Simon", "Siméon", "Sixte", "Stanislas", "Stéphane", "Stephan", "Sylvain", "Sylvestre", "Tancrède", "Tanguy", "Taurin", "Théodore"], k=n_owner)
+    email = [f"{p}.{n}@example.com".lower() for p, n in zip(first_name, last_name)]
+    phone = ["06" + "".join(rd.choices(string.digits, k=8)) for _ in range(n_owner)]
+    owner_id = [last_name[i] + first_name[i] + str(nrd.randint(0, 1e8)) for i in range(n_owner)]
+
+    picture = ["/pictures/owners/" + idd for idd in owner_id]
+    bio = ["Bio de " + p + " " + n for p, n in zip(first_name, last_name)]
+    
+    df_owner = pd.DataFrame({
+        'owner_id': owner_id,
+        'last_name': last_name,
+        'first_name': first_name,
+        'email': email,
+        'phone': phone,
+        'picture': picture,
+        'bio': bio
+    })
+    df_owner.to_csv("./data_out/df_Owner.csv", index=False)
+
+
+
 
 if dog :
     # load & diplay
@@ -31,220 +64,231 @@ if dog :
     n_dog = len(df_quaggle_dog['Breed'])
     print(df_quaggle_dog.head())
     
+    random_ids = np.random.randint(1e5, 1e15, size=n_dog)
+    chip_id = [str(250) + f"{r:015d}" for r in random_ids ]
+    owner_id = nrd.choice(owner_id, n_dog, replace=True)
+    name = nrd.choice(["John", "Milou", "Rex", "Idefix", 'Max','Charlie','Bella','Poppy','Daisy','Buster','Alfie','Millie','Molly','Rosie','Buddy','Barney','Lola','Roxy','Ruby','Tilly','Bailey','Marley','Tia', 'Bernie'], n_dog)
+
     
     # init fields
-    id_dog = np.arange(0, n_dog, 1)
-    chip_number = np.zeros(n_dog, )
-    owner_id = np.zeros(n_dog, )
-    name = nrd.choice(["John", "Twingo", "Bertholet"], n_dog)
+    dog_id = [f"{name[j]}_{chip_id[j]}" for j in range(n_dog)]
+    
+    
     weight = df_quaggle_dog['Weight (kg)']
     breed = df_quaggle_dog['Breed']
-    photo = nrd.randint(0, 3, n_dog)
+    picture = ["/pictures/dogs/" + idd for idd in dog_id]
     birthdate = 2025 - np.round(df_quaggle_dog['Age (Years)'])
-    optimal_tour_duration = np.floor(weight/3 + nrd.rand(n_dog)*weight/10)
-    sportivity = nrd.choice(["High", "Medium", "Low"], n_dog)
+    optimal_tour_duration = np.floor( (weight + (nrd.rand(n_dog)+1)*weight/2)/3 )
+    athleticism = nrd.randint(0, 10, size=n_dog)
     
     
     # file in fields & save
-    df_dog = pd.DataFrame({'id_dog': id_dog,
-                          'chip_number': chip_number,
+    df_dog = pd.DataFrame({'dog_id': dog_id,
+                          'chip_id': chip_id,
                           'owner_id': owner_id,
                           'name': name,
                           'weight': weight,
                           'breed': breed,
-                          'photo': photo,
+                          'picture': picture,
                           'birthdate': birthdate,
                           'optimal_tour_duration': optimal_tour_duration,
-                          'sportivity': sportivity
+                          'athleticism': athleticism
               })
     
-    df_dog.to_csv("./data_out/df_dog.csv")
+    df_dog.to_csv("./data_out/df_Dog.csv")
 
 
-if propriétaire :
-    # --- propriétaires 
-    n_propriétaire = 100  # exemple: 100 propriétaires
+
+
+if walker :
+    # --- walkers 
+    n_walker = 50
     
-    id_propriétaire = np.arange(0, n_propriétaire, 1)
-    nom = rd.choices(["Martin", "Bernard", "Dubois", "Thomas", "Robert"], k=n_propriétaire)
-    prenom = rd.choices(["Jean", "Marie", "Pierre", "Sophie", "Luc"], k=n_propriétaire)
-    mail = [f"{p}.{n}@example.com".lower() for p, n in zip(prenom, nom)]
-    phone = ["06" + "".join(rd.choices(string.digits, k=8)) for _ in range(n_propriétaire)]
-    photo = nrd.randint(0, 5, n_propriétaire)
-    bio = ["Bio de " + p + " " + n for p, n in zip(prenom, nom)]
-    
-    df_propriétaire = pd.DataFrame({
-        'id_propriétaire': id_propriétaire,
-        'nom': nom,
-        'prenom': prenom,
-        'mail': mail,
-        'phone': phone,
-        'photo': photo,
-        'bio': bio
-    })
-    df_propriétaire.to_csv("./data_out/df_propriétaire.csv", index=False)
+
+    last_name = rd.choices(["Dupont", "Lambert", "Petit", "Moreau", "Girard"], k=n_walker)
+    first_name = rd.choices(["Nicolas", "Camille", "Julien", "Élodie", "Thomas"], k=n_walker)
+    walker_id = [last_name[i] + first_name[i] + str(nrd.randint(0, 1e8)) for i in range(n_walker)]
+
+    picture = ["/pictures/walkers/" + idd for idd in walker_id]
+    bio = ["Bio de " + p + " " + n for p, n in zip(first_name, last_name)]
+    profil_verifie = rd.choices([True, False], k=n_walker)
+    email = [f"{p}.{n}@laposte.net".lower() for p, n in zip(first_name, last_name)]
+    phone = ["07" + "".join(rd.choices(string.digits, k=8)) for _ in range(n_walker)]
+    birth_date = nrd.normal(1990, 20, size=n_walker)
+    rib = ["FR76" + "".join(rd.choices(string.digits, k=23)) for _ in range(n_walker)]
 
 
-
-if promeneur :
-    # --- promeneurs 
-    n_promeneur = 50  # exemple: 50 promeneurs
     
-    id_promeneur = np.arange(0, n_promeneur, 1)
-    nom = rd.choices(["Dupont", "Lambert", "Petit", "Moreau", "Girard"], k=n_promeneur)
-    prenom = rd.choices(["Nicolas", "Camille", "Julien", "Élodie", "Thomas"], k=n_promeneur)
-    photo = nrd.randint(0, 5, n_promeneur)
-    bio = ["Bio de " + p + " " + n for p, n in zip(prenom, nom)]
-    profil_verifie = rd.choices([True, False], k=n_promeneur)
-    mail = [f"{p}.{n}@promeneur.com".lower() for p, n in zip(prenom, nom)]
-    phone = ["07" + "".join(rd.choices(string.digits, k=8)) for _ in range(n_promeneur)]
-    date_naissance = [rd.randint(1965, 2005) for _ in range(n_promeneur)]
-    rib = ["FR76" + "".join(rd.choices(string.digits, k=23)) for _ in range(n_promeneur)]
-    
-    df_promeneur = pd.DataFrame({
-        'id_promeneur': id_promeneur,
-        'nom': nom,
-        'prenom': prenom,
-        'photo': photo,
+    df_walker = pd.DataFrame({
+        'walker_id': walker_id,
+        'last_name': last_name,
+        'first_name': first_name,
+        'picture': picture,
         'bio': bio,
         'profil_verifie': profil_verifie,
-        'mail': mail,
+        'email': email,
         'phone': phone,
-        'date_naissance': date_naissance,
+        'date_naissance': birth_date,
         'rib': rib
     })
-    df_promeneur.to_csv("./data_out/df_promeneur.csv", index=False)
+    df_walker.to_csv("./data_out/df_Walker.csv", index=False)
 
 
 
 
 
-if promeneur_dispo :
-    # --- promeneur_dispo 
-    n_dispo = 200  # exemple: 200 disponibilités
+if walker_availability :
+    # --- walker_availability 
+    n_availability = 200
+    walker_id = rd.choices(df_walker['walker_id'], k=n_availability)
+
+    address = rd.choices(["Paris", "Lyon", "Marseille", "Toulouse", "Bordeaux"], k=n_availability)
     
-    id_promeneur = rd.choices(df_promeneur['id_promeneur'], k=n_dispo)
-    adresse = rd.choices(["Paris", "Lyon", "Marseille", "Toulouse", "Bordeaux"], k=n_dispo)
-    horodate_debut_disponibilite = pd.date_range(start="2025-10-01", periods=n_dispo, freq="H").tolist()
-    horodate_fin_disponibilite = pd.date_range(start="2025-10-02", periods=n_dispo, freq="H").tolist()
+    # generate availability
+    dr = pd.date_range("2025-10-10", "2026-02-02", freq="h")
+    # random start datetime
+    start_datetime = pd.to_datetime(np.sort(np.random.permutation(np.arange(len(dr)))[:n_availability]))
+    # random start datetime + [1-6] h of availability
+    end_datetime = start_datetime + pd.to_timedelta(nrd.randint(1, 6, size=n_availability), unit="h")
+
+    #start_datetime = pd.date_range(start="2025-10-01", periods=n_availability, freq="h").tolist()
+    #end_datetime = pd.date_range(start="2025-10-02", periods=n_availability, freq="h").tolist()
     
-    df_promeneur_dispo = pd.DataFrame({
-        'id_promeneur': id_promeneur,
-        'adresse': adresse,
-        'horodate_debut_disponibilite': horodate_debut_disponibilite,
-        'horodate_fin_disponibilite': horodate_fin_disponibilite
+    availability_id = [walker_id[i] + str(start_datetime[i]) for i in range(n_availability)]
+
+    df_walker_availability = pd.DataFrame({
+        'walker_id': walker_id,
+        'address': address,
+        'availability_id':availability_id,
+        'start_datetime': start_datetime,
+        'end_datetime': end_datetime
     })
-    df_promeneur_dispo.to_csv("./data_out/df_promeneur_dispo.csv", index=False)
+    df_walker_availability.to_csv("./data_out/df_WalkerAvailability.csv", index=False)
 
 
-if promenade_requests :
-    # --- Promenades_requests 
+
+if walk_requests :
+    # --- walks_requests 
     n_request = 150
     
-    id_chien = rd.choices(np.arange(0, n_dog, 1), k=n_request)  # suppose n_dog défini plus haut
-    id_promenade = np.arange(0, n_request, 1)
-    adresse = rd.choices(["Paris", "Lyon", "Marseille", "Toulouse", "Bordeaux"], k=n_request)
-    horodate_debut_ideal = pd.date_range(start="2025-10-03", periods=n_request, freq="H").tolist()
-    duree_request = [rd.randint(30, 120) if rd.random() > 0.5 else np.nan for _ in range(n_request)]
-    distance_request = [rd.randint(1, 10) if rd.random() > 0.5 else np.nan for _ in range(n_request)]
-    id_promeneur_favori = [rd.choice(df_promeneur['id_promeneur']) if rd.random() > 0.7 else np.nan for _ in range(n_request)]
-    remuneration_predite = [rd.randint(10, 50) for _ in range(n_request)]
+    dog_id = rd.choices(df_dog['dog_id'], k=n_request) 
+    address = rd.choices(["Paris", "Lyon", "Marseille", "Toulouse", "Bordeaux"], k=n_request)
+    ideal_start_datetime = pd.date_range(start="2025-10-03", periods=n_request, freq="h").tolist()
+    duration_request = [nrd.randint(30, 120) if nrd.random() > 0.6 else np.nan for _ in range(n_request)]
+    distance_request = [nrd.randint(1, 10) if nrd.random() > 0.6 else np.nan for _ in range(n_request)]
+    walk_id = [dog_id[i] + str(ideal_start_datetime[i]) for i in range(n_request)]
+
+    favorite_walker_id = [nrd.choice(df_walker['walker_id']) if rd.random() > 0.7 else np.nan for _ in range(n_request)]
+    predicted_payment = [nrd.randint(10, 50) for _ in range(n_request)]
     
-    df_promenades_requests = pd.DataFrame({
-        'id_chien': id_chien,
-        'id_promenade': id_promenade,
-        'adresse': adresse,
-        'horodate_debut_ideal': horodate_debut_ideal,
-        'duree_request': duree_request,
+    df_walks_requests = pd.DataFrame({
+        'dog_id': dog_id,
+        'walk_id': walk_id,
+        'address': address,
+        'ideal_start_datetime': ideal_start_datetime,
+        'duration_request': duration_request,
         'distance_request': distance_request,
-        'id_promeneur_favori': id_promeneur_favori,
-        'remuneration_predite': remuneration_predite
+        'favorite_walker_id': favorite_walker_id,
+        'predicted_payment': predicted_payment
     })
-    df_promenades_requests.to_csv("./data_out/df_promenades_requests.csv", index=False)
+    df_walks_requests.to_csv("./data_out/df_WalksRequests.csv", index=False)
 
 
-
-
-if promenade_passées :
-    # --- promenades_passées
-    n_promenade = 100 
+if past_walks :
+    # --- past walks
+    n_walk = n_past_walk 
     
-    id_promenade = np.arange(0, n_promenade, 1)
-    id_chien = rd.choices(np.arange(0, n_dog, 1), k=n_promenade)
-    id_promeneur = rd.choices(df_promeneur['id_promeneur'], k=n_promenade)
-    distance = nrd.randint(1, 15, n_promenade)
-    horodate_debut = pd.date_range(start="2025-09-01", periods=n_promenade, freq="H").tolist()
-    horodate_fin = pd.date_range(start="2025-09-02", periods=n_promenade, freq="H").tolist()
-    photo = nrd.randint(0, 5, n_promenade)
-    avis_chien_id = [rd.choice([None, np.nan, rd.randint(0, 100)]) for _ in range(n_promenade)]
-    avis_promeneur_id = [rd.choice([None, np.nan, rd.randint(0, 100)]) for _ in range(n_promenade)]
+    dog_id = rd.choices(df_dog['dog_id'], k=n_walk) 
+    walker_id = rd.choices(df_walker['walker_id'], k=n_walk)
+    distance = nrd.randint(1, 15, n_walk)
+    start_datetime = pd.date_range(start="2025-09-01", periods=n_walk, freq="h").tolist()
+    end_datetime = pd.date_range(start="2025-09-02", periods=n_walk, freq="h").tolist()
+
+    walk_id = [dog_id[i] + '-' + walker_id[i] + str(start_datetime[i]) for i in range(n_walk)]
+    picture = ["/pictures/past_walks/" + idd for idd in walk_id]
+
+    dog_review_id = [walk_id[i] + "_dogreview" for i in range(n_walk)]
+    walker_review_id = [walk_id[i] + "_walkerreview" for i in range(n_walk)]
+
+
     
-    df_promenades_passées = pd.DataFrame({
-        'id_promenade': id_promenade,
-        'id_chien': id_chien,
-        'id_promeneur': id_promeneur,
+    df_past_walks = pd.DataFrame({
+        'walk_id': walk_id,
+        'dog_id': dog_id,
+        'walker_id': walker_id,
         'distance': distance,
-        'horodate_debut': horodate_debut,
-        'horodate_fin': horodate_fin,
-        'photo': photo,
-        'avis_chien_id': avis_chien_id,
-        'avis_promeneur_id': avis_promeneur_id
+        'start_datetime': start_datetime,
+        'end_datetime': end_datetime,
+        'picture': picture,
+        'dog_review_id': dog_review_id,
+        'walker_review_id': walker_review_id
     })
-    df_promenades_passées.to_csv("./data_out/df_promenades_passées.csv", index=False)
+    df_past_walks.to_csv("./data_out/df_past_walks.csv", index=False)
 
 
 
-if note_promeneur :
-    # --- notes_promeneurs
-    n_notes_promeneur = 80 
+if walker_review :
+    # --- walker_review
+    n_walker_review = n_past_walk 
     
-    id_promenade = rd.choices(df_promenades_passées['id_promenade'], k=n_notes_promeneur)
-    id_promeneur = rd.choices(df_promeneur['id_promeneur'], k=n_notes_promeneur)
-    note = nrd.randint(1, 5, n_notes_promeneur)
-    commentaire = ["Commentaire pour " + str(i) for i in range(n_notes_promeneur)]
-    
-    df_notes_promeneurs = pd.DataFrame({
-        'id_promenade': id_promenade,
-        'id_promeneur': id_promeneur,
-        'note': note,
-        'commentaire': commentaire
+    walk_id = rd.choices(df_past_walks['walk_id'], k=n_walker_review)
+    walker_id = rd.choices(df_walker['walker_id'], k=n_walker_review)
+    rating = nrd.choice(np.arange(1, 6), size=n_walker_review, replace=True, p=[0.1, 0.05, 0.05, 0.3, 0.5])
+    comment = ["Commentaire pour " + str(i) for i in range(n_walker_review)]
+    review_id = [walk_id[i] + "_walkerreview" for i in range(n_walker_review)]
+
+    df_ratings_walkers = pd.DataFrame({
+        'review_id': review_id,
+        'walk_id': walk_id,
+        'walker_id': walker_id,
+        'rating': rating,
+        'comment': comment
     })
-    df_notes_promeneurs.to_csv("./data_out/df_notes_promeneurs.csv", index=False)
+    df_ratings_walkers.to_csv("./data_out/df_WalkerReview.csv", index=False)
 
-if notes_chien :
-    # --- notes_chien 
-    n_notes_chien = 80
+if dog_review :
+    # --- dog_review 
+    n_dog_review = n_past_walk
     
-    id_promenade = rd.choices(df_promenades_passées['id_promenade'], k=n_notes_chien)
-    id_chien = rd.choices(np.arange(0, n_dog, 1), k=n_notes_chien)
-    note = nrd.randint(1, 5, n_notes_chien)
-    commentaire = ["Commentaire pour chien " + str(i) for i in range(n_notes_chien)]
-    
-    df_notes_chien = pd.DataFrame({
-        'id_promenade': id_promenade,
-        'id_chien': id_chien,
-        'note': note,
-        'commentaire': commentaire
+    walk_id = rd.choices(df_past_walks['walk_id'], k=n_dog_review)
+    dog_id = rd.choices(np.arange(0, n_dog, 1), k=n_dog_review)
+    rating = nrd.choice(np.arange(1, 6), size=n_walker_review, replace=True, p=[0.1, 0.05, 0.05, 0.3, 0.5])
+    comment = ["comment pour chien " + str(i) for i in range(n_dog_review)]
+    review_id = [walk_id[i] + "_dogreview" for i in range(n_dog_review)]
+
+    df_dog_review = pd.DataFrame({
+        'review_id': review_id,
+        'walk_id': walk_id,
+        'dog_id': dog_id,
+        'rating': rating,
+        'comment': comment
     })
-    df_notes_chien.to_csv("./data_out/df_notes_chien.csv", index=False)
+    df_dog_review.to_csv("./data_out/df_DogReview.csv", index=False)
 
 
 
-if paiements_propriétaires :
-    # --- paiements_propriétaires 
-    n_paiements = 90
-    
-    id_promenade = rd.choices(df_promenades_passées['id_promenade'], k=n_paiements)
-    montant = [rd.randint(5, 40) for _ in range(n_paiements)]
-    horodate_paiement = pd.date_range(start="2025-09-10", periods=n_paiements, freq="H").tolist()
-    moyen_paiement = rd.choices(["CB", "Paypal", "Virement"], k=n_paiements)
-    statut_paiement = rd.choices(["Payé", "En attente", "Échoué"], k=n_paiements)
-    
-    df_paiements_propriétaires = pd.DataFrame({
-        'id_promenade': id_promenade,
-        'montant': montant,
-        'horodate_paiement': horodate_paiement,
-        'moyen_paiement': moyen_paiement,
-        'statut_paiement': statut_paiement
+
+
+
+
+
+if owner_payment :
+    # --- owner_payment 
+    n_payment = n_past_walk
+    # payment is supposed unique for a walk
+    walk_id = nrd.choice(df_past_walks['walk_id'], n_payment, replace=False)
+    amount = [rd.randint(3, 40) for _ in range(n_payment)] # should go look in the walk_request, predicted_payment
+    payment_datetime = pd.date_range(start="2025-09-10", periods=n_payment, freq="h").tolist()
+    payment_method = rd.choices(["DebitCard", "CreditCard"], k=n_payment)
+    payment_status = rd.choices(["complete", "pending", "failed"], k=n_payment)
+    payment_id = [walk_id[i] + "_000" for i in range(n_payment)]
+
+    df_owner_payment = pd.DataFrame({
+        'payment_id':payment_id,
+        'walk_id': walk_id,
+        'amount': amount,
+        'payment_datetime': payment_datetime,
+        'payment_method': payment_method,
+        'payment_status': payment_status
     })
-    df_paiements_propriétaires.to_csv("./data_out/df_paiements_propriétaires.csv", index=False)
+    df_owner_payment.to_csv("./data_out/df_OwnerPayment.csv", index=False)
