0e005ee2743b74d23a721285abe331a273352f6f
Pouré Thomas <thomas@MacBook-Air-de-Poure.local>
2025-10-21 22:20:08 +0200

    merge du main blblblblbl

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ from Transform import clean
 from EL_DB_Toutour import init_db, load_csv_to_db
 from HotDog import run_app
 import time
-pause_time = 2
 
 if __name__ == "__main__":
     print('Generating initial data ...')
     generate_all_v2.generate_all_csv()
-    make_noise.make_noise(pc_null=1, pc_nullrows=0, pc_exagerated=0, directory_out="./data_out", except_ids=True)
-    time.sleep(pause_time)
+    make_noise.make_noise()
+    time.sleep(2)
 
     print('Cleaning initial data ...')
-
-    clean.clean_all()
-    time.sleep(pause_time)
+    clean.clean_all_relational()
+    time.sleep(2)
 
     print('Loading initial data into database ...')
     init_db.init_db()
     load_csv_to_db.main()
-    time.sleep(pause_time)
+    time.sleep(2)
     
     print('Hot Dog !')
     run_app.run_app()
